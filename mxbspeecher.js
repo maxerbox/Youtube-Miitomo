b3718658361ccb17a76d7153e4ec013b935e3e74
maxerbox <dualprodu@gmail.com>
2016-05-05 10:29:41 +0200

    Added Preload Function

--- mxbspeecher.js
+++ mxbspeecher.js
@@ -1,49 +1,123 @@
 function MxbSpeech() {
-    this.defaultLang = "Manon";
+     var audioStack = {};
+    this.defaultLang = "eurfrenchfemale";
+    this.callback = function(){};
+    this.loadcall=function(data){};
+    this.changecall=function(){};
     this.player = new Audio();
-    this.speak = function(text,_lang) {
+    this.preload = function(text,_args) {
         if (text ==="")
-            throw new Exception("Invalid text input");
+            throw "Invalid text input";
         var lang = this.defaultLang;
-        if (typeof _lang!='undefined')
-            lang=_lang;
-        var http = new XMLHttpRequest();
-        var url = "https://www.acapela-group.com/demo-tts/DemoHTML5Form_V2_fr.php?langdemo=V";
-        var params = "MyLanguages=sonid15&0=Leila&1=Laia&2=Eliska&3=Mette&4=Zoe&5=Jasmijn&6=Tyler&7=Deepa&8=Rhona&9=Rachel&10=Sharon&11=Hanna&12=Sanna&13=Justine&14=Louise&MySelectedVoice="+lang+"&16=Claudia&17=Dimitris&18=Fabiana&19=Sakura&20=Minji&21=Lulu&22=Bente&23=Monika&24=Marcia&25=Celia&26=Alyona&27=Biera&28=Ines&29=Rodrigo&30=Elin&31=Samuel&32=Kal&33=Mia&34=Ipek&MyTextForTTS="+text+"&t=1&SendToVaaS=";
-        http.open("POST", url, true);
-        http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        http.onreadystatechange = function() {
-            if(http.readyState == 4 && http.status == 200) {
-                var evt = new CustomEvent("SpeechStatChange",{code:200,message:"Voice Requested"});
-                window.dispatchEvent(evt);
-                var response = http.responseText;
-                var uri = response.substring(str.lastIndexOf("'",response.indexOf("myPhpVar"))+1,str.lastIndexOf("'"));
-                console.log(uri);
-                if(uri == -1)
-                    throw new Exception("Could not get the uri on the server, check your args!");
-                this.player.src = uri;
-                this.player.onended = function() {
-                    var evt = new CustomEvent("SpeechFinish");
-                    window.dispatchEvent(evt);
-                };
-                this.player.play();
-            } else {
-                throw new Exception("Check your connection, couldn't contact th server");
-            }
+        if (typeof _args.lang!='undefined')
+            lang=_args.lang;
+        var args = _args;
+        if (typeof args == 'undefined')
+            args = {speed:0,pitch:100,loadcall:function(){},callback:function(){}};
+        if (typeof args.speed == 'undefined')
+            args.speed=0;
+        if(typeof args.pitch == 'undefined')
+            args.pitch=100;
+        if (typeof args.changecall == 'undefined' || typeof args.changecall != 'function')
+            args.changecall=this.changecall;
+        if (typeof args.loadcall == 'undefined' || typeof args.loadcall != 'function')
+            args.loadcall=this.loadcall;
+        else
+            this.player.removeEventListener("canplay",this.loadcall);
+        if(typeof args.callback == 'undefined' || typeof args.callback != 'function')
+            args.callback=this.callback;
+        else
+            this.player.removeEventListener("ended",this.callback);
+
+        var uri = encodeURI("https://api.ispeech.org/api/rest?apikey=34b06ef0ba220c09a817fe7924575123&action=convert&voice="+lang+"&speed="+args.speed+"&pitch="+args.pitch+"&text="+text);
+        var audio = new Audio(uri);
+        audio.oncanplaythrough = function(evt) {
+            audioStack[text] = audio;
+            console.info("Loaded: "+text);
         };
-        http.send(params);
+        audio.load();
+    };
+    this.removeLoaded = function(key) {
+        if(audioStack.hasOwnProperty(text))
+            delete map[key];
+        else
+            console.log(key + " is not in the audioStack !");
+    };
+    this.clearLoaded = function(){
+        audioStack = {};
+    };
+    this.setVolume = function(volume) {
+        console.log(volume);
+        this.player.volume=volume/100;
+    };
+    this.isPlaying = function() {
+        return !this.player.paused;
+    };
+    this.setEventListener = function(_args) {
+        var args = _args;
+        if(typeof args.loadcall != 'undefined')
+            this.loadcall = args.loadcall;
+        if(typeof args.callback != 'undefined')
+            this.callback = args.callback;
+        if (typeof args.changecall == 'undefined' || typeof args.changecall != 'function')
+            this.changecall=args.changecall;
+    };
+    this.speak = function(text,_args) {
+        if (text ==="")
+            throw "Invalid text input";
+        var lang = this.defaultLang;
+        if (typeof _args.lang!='undefined')
+            lang=_args.lang;
+        var args = _args;
+        if (typeof args == 'undefined')
+            args = {speed:0,pitch:100,loadcall:function(){},callback:function(){}};
+        if (typeof args.speed == 'undefined')
+            args.speed=0;
+        if(typeof args.pitch == 'undefined')
+            args.pitch=100;
+        if (typeof args.changecall == 'undefined' || typeof args.changecall != 'function')
+            args.changecall=this.changecall;
+        if (typeof args.loadcall == 'undefined' || typeof args.loadcall != 'function')
+            args.loadcall=this.loadcall;
+        else
+            this.player.removeEventListener("canplay",this.loadcall);
+        if(typeof args.callback == 'undefined' || typeof args.callback != 'function')
+            args.callback=this.callback;
+        else
+            this.player.removeEventListener("ended",this.callback);
+        if(this.callback != args.callback ||  this.loadcall != args.loadcall || this.changecall != args.changecall) {
+            this.callback = args.callback;
+            this.loadcall = args.loadcall;
+            this.changecall = args.changecall;
+        }
+        var uri = encodeURI("https://api.ispeech.org/api/rest?apikey=34b06ef0ba220c09a817fe7924575123&action=convert&voice="+lang+"&speed="+args.speed+"&pitch="+args.pitch+"&text="+text);
+        if (this.player.src != uri)
+            this.changecall();
+        if(!audioStack.hasOwnProperty(text)){
+             console.log(uri);
+            console.log(Object.keys(audioStack).length);
+            this.player.src=uri;
+        } else {
+            console.log(audioStack[text]);
+            this.player = audioStack[text];
+           this.loadcall({target:this.player}); //TRIGGER LOADCALL;
+        }
+        this.player.addEventListener("canplay",this.loadcall,false);
+        this.player.addEventListener("ended", this.callback,false);
+        this.player.play();
+        return this.player.duration;
     };
     this.setDefault=function(_lang) {
-        if(lang=="")
-            throw new Exception("Invalid blank lang");
+        if(lang==="")
+            throw "Invalid blank lang";
         this.defaultLang = _lang;
         var evt = new CustomEvent("SpeechStatChange",{code:4,message:"Changed Default Lang"});
         window.dispatchEvent(evt);
     };
     this.cancel = function() {
-        this.player.pause();
-        this.player.src="";
-        var evt = new CustomEvent("SpeechStatChange",{code:4,message:"Cancel speech"});
+        this.pause();
+
+        var evt = new CustomEvent("SpeechStatChange",{code:5,message:"Cancel speech"});
         window.dispatchEvent(evt);
     };
     this.resume = function() {
